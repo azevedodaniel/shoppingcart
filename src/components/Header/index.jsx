e76e49e55a8916de652ca76cb2ce18a1f2444c41
Daniel Azevedo <daniel.ellerazevedo@gmail.com>
2023-08-30 14:37:46 -0300

    creation of header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import CartButton from '../CartButton';
+import SearchBar from '../SearchBar';
+
+import './styles.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="container">
+        
+        <SearchBar />
+        <CartButton />
+
+      </div>
+    </header>
+  );
+}
+
+export default Header;
