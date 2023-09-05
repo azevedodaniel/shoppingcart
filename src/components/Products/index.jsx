49df1cd24fe16398dda692a6fb3305e7f0b892b7
Daniel Azevedo <daniel.ellerazevedo@gmail.com>
2023-09-04 22:46:15 -0300

    functional search bar and finished cart menu

--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -1,28 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useContext } from 'react';
 import fetchProducts from '../../api/fetchProducts';
+import AppContext from '../../context/AppContext';
+import Loading from '../Loading';
 import ProductCard from '../ProductCard';
 
 import './styles.css';
 
 function Products() {
 
-  const [products, setProducts] = useState([]);
+  const { products, setProducts, loading, setLoading } = useContext(AppContext);
 
   useEffect(() => {
     fetchProducts('iphone').then((response) => {
       setProducts(response);
+      setLoading(false);
     });
 
   }, []);
 
   return ( 
-    <section className="products container">
-
-      {
-        products.map((product) => <ProductCard key={product.id} data={product} />)
-      }
-
-    </section>
+    (loading && <Loading />) || (
+      <section className="products container">
+        {products.map((product) => <ProductCard key={product.id} data={product} />)}
+      </section>
+    )
   );
 }
 
